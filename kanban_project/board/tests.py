72cf5fc4e4329bf202d99d302829c1aee693d2e9
Barnabas Gonda <barnabas.gonda@googlemail.com>
2024-04-24 22:03:12 +0200

    test

--- kanban_project/board/tests.py
+++ kanban_project/board/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from .models import Task
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class MyModelTestCase(TestCase):
+    def setUp(self):
+        user = User.objects.create(username='testuser')
+        Task.objects.create(title="Test", description="Test description", created_by=user)
+
+    def test_my_model(self):
+        obj = Task.objects.get(title="Test")
+        self.assertEqual(obj.title, "Test")
